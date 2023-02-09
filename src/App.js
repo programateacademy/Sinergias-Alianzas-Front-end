ec97c4dfcce137b294f0c465959e5b11c5a3aa40
Edward Melo <edwardalejandromelog@gmail.com>
2023-02-09 17:03:20 -0500

    Profiles add

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Form from "./form/Form";
 function App() {
   return (
     <div>
-      <h1>Proyecto Sinergias</h1>
       <Form/>
     </div>
   );
--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,98 +1,110 @@
 import React from 'react'
 import "./Form.css"
 import libro from "./img/Libro.png"
+import { BiEdit } from 'react-icons/bi';
+import { RiDeleteBin2Line } from 'react-icons/ri';
+
+
 const Form = () => {
   return (
     <>
-    <section className='partForm'>
-      
-    <h1 className='texttitlee'> Formulario</h1>
-      <div>
-        
-        <div className='containerform'> 
-          <div className='pict'> 
-          
-            <img src={libro} alt="imagen libro" />
+      <section className='partForm'>
 
-            <div className='info'>
-              <div className=''>
-                <h1>
-                Primer apellido
-                </h1>
-                <input 
-                type="text" 
-                className='texth2'/>
+        <h1 className='texttitlee'> Formulario</h1>
+        <div>
+
+          <div className='containerform'>
+            <div className='pict'>
+
+              <img src={libro} alt="imagen libro" />
+
+              <div className='info'>
+                <div className=''>
+                  <h1>
+                    Primer apellido
+                  </h1>
+                  <input
+                    type="text"
+                    className='texth2' />
+                </div>
+                <div className=''>
+                  <h1>
+                    Segundo apellido
+                  </h1>
+                  <input
+                    type="text"
+                    className='texth2' />
+                </div>
+                <div className=''>
+                  <h1>
+                    Primer nombre
+                  </h1>
+                  <input
+                    type="text"
+                    className='texth2' />
+                </div>
+                <div className=''>
+                  <h1>
+                    Segundo nombre
+                  </h1>
+                  <input
+                    type="text"
+                    className='texth2' />
+                </div>
+                <div className=''>
+                  <h1>
+                    Correo
+                  </h1>
+                  <input
+                    type="text"
+                    className='texth2' />
+                </div>
+                <div className=''>
+                  <h1>
+                    Contraseña
+                  </h1>
+                  <input
+                    type="text"
+                    className='texth2' />
+                </div>
+                <div className='add'>
+                  <button className='butadd'> Añadir</button>
+                </div>
+              <div className='textProfile'> 
+                <h1>Perfiles agregados</h1> 
               </div>
-              <div className=''>
-                <h1>
-                Segundo apellido
-                </h1>
-                <input 
-                type="text" 
-                className='texth2'/>
-              </div>
-              <div className=''>
-                <h1>
-                Primer nombre
-                </h1>
-                <input 
-                type="text" 
-                className='texth2'/>
-              </div>
-              <div className=''>
-                <h1>
-                Segundo nombre
-                </h1>
-                <input 
-                type="text" 
-                className='texth2'/>
-              </div>
-              <div className=''>
-                <h1>
-                Correo
-                </h1>
-                <input 
-                type="text" 
-                className='texth2'/>
-              </div>
-              <div className=''>
-                <h1>
-                Contraseña
-                </h1>
-                <input 
-                type="text" 
-                className='texth2'/>
-              </div>
-              <div className='add'>
-              <button className='butadd'> Añadir</button>
+                <div >
+                    <div className='profiles'>
+                      <h1>
+                        nombre y apellido
+                      </h1>
+                      <button>
+                        <BiEdit/>
+                      </button>
+                      <button>
+                        <RiDeleteBin2Line/>
+                      </button>
+                    </div>
+                    <div className='profiles'>
+                      <h1>
+                        nombre y apellido
+                      </h1>
+                      <button>
+                        <BiEdit/>
+                      </button>
+                      <button>
+                        <RiDeleteBin2Line/>
+                      </button>
+                    </div>
+                </div>
               </div>
             </div>
+
+
           </div>
-
-          
+        
         </div>
-        {/* <table class="default">
-          <tr>
-            <th scope="row">Caracteristicas</th>
-            <th>Primer Nombre</th>
-            <th>Segundo Nombre</th>
-            <th>Primer apellido</th>
-            <th>Segundo apellido</th>
-            <th>Correo</th>
-          </tr>
-          <tr>
-            <th>Usuario</th>
-            <td>Soleado</td>
-            <td>Mayormente soleado</td>
-            <td>Parcialmente nublado</td>
-            <td>fdsf</td>
-            <td>fdasfads</td>
-          </tr>
-          
-        </table> */}
-
-      </div>
-    </section>
+      </section>
     </>
   )
 }

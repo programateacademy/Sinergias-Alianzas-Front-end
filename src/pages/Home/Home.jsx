b76f79cb55cb48f9ac9dc33acd63714d546cb187
Nisanech <57728976+Nisanech@users.noreply.github.com>
2023-02-12 18:59:18 -0500

    Se agrega listado de componentes (cursos) en la vista principal

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import ListCourses from '../../components/ListCourses/ListCourses'
+
 const Home = () => {
   return (
     <>
       <h1>Dashboard Administrador</h1>
+
+      <ListCourses/>
     </>
   );
 };

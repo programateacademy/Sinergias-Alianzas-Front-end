d6d7e30bf847712da40a98ffc56a6b791057b5f2
Edward Melo <edwardalejandromelog@gmail.com>
2023-02-14 17:52:32 -0500

    styles popups

--- src/components/RowTableUsers/RowTableUsers.jsx
+++ src/components/RowTableUsers/RowTableUsers.jsx
@@ -26,32 +26,34 @@ import {
 import Buttonedit from"../Buttonedit/buttonedit";
 import Buttondelete from "../../components/buttondelete/buttondelete"
 
-
+//style
+import "../RowTableUsers/RowTableUsers.css"
 const RowTableUsers = () => {
   
  
   return (
     
     
-    <tr>
+    <tr className="tableinfo2">
       <th scope="row">1</th>
       <td>Nombre usuario</td>
       <td>usuario@correo.com</td>
       <td>Colaborador</td>
       <td>
-        <Button >
+        <Button color="">
         <Buttonedit>
         <FaEdit color="green" size={15}  />
         </Buttonedit>
         </Button>
 
-        <Button>
+        <Button color="">
         <Buttondelete>
         <FaTrash color="red" size={15} />
         </Buttondelete>
         </Button>
       </td>
     </tr>
+    
     
   );
 };

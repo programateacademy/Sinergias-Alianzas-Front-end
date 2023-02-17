f81fa80741985ef6bd89758d3ada0d3145999bbe
Karen Andrea Niño Angulo <daka201828@gmail.com>
2023-02-17 11:34:44 -0500

    Undo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,8 +9,6 @@ import searchButton from "../../components/ListCourses/Assets/searchButton.png";
 import uploadButton from "../../components/ListCourses/Assets/uploadButton.png";
 
 // Componentes
-import ListCourses from "../../components/ListCourses/ListCourses";
-import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
 import { useNavigate, Link } from "react-router-dom";
 import CardComponent from "../../components/CardComponent/CardComponent";
 import { Spinner } from "reactstrap";
@@ -35,8 +33,7 @@ const Home = () => {
   if (loading) {
     return <Spinner>Cargando</Spinner>;
   }
-  //* Hook personalizado para redireccionar el usuario si la sesión expira
-  useRedirectLoggedOutUser("/");
+
   return (
     <>
       <div className="containerTitle">

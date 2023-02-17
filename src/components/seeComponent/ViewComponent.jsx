831827f5e7704e6fbd704f9b4e57e499b2606147
Cristian Betancourt <cristian.camilo.betancourt.rin@gmail.com>
2023-02-17 16:20:16 -0500

    Push to pull

--- src/components/seeComponent/ViewComponent.jsx
+++ src/components/seeComponent/ViewComponent.jsx
@@ -1,54 +1,47 @@
-import React from 'react'
+import React from "react";
 import { Card, CardBody, CardTitle, CardText } from "reactstrap";
 import "./seeComponent.css";
-
 
 import Metodologia from "../../components/AddComponent/assest/Rectangle25.png";
 import Formato from "../../components/AddComponent/assest/Rectangle26.png";
 import Diagnostico from "../../components/AddComponent/assest/Rectangle27.png";
 import Herramientas from "../../components/AddComponent/assest/Rectangle28.png";
 import Material from "../../components/AddComponent/assest/Rectangle29.png";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const ViewComponent = ({
-    compTitulo,
-    compImgPpal,
-    compDefinicion,
-    compVideo,
-    compDescripcion,
-    compImg1,
-    compImg2,
-    compImg3,
-    compObjetivo1,
-    compObjetivo2,
-    compObjetivo3,
-    compLineaTrabajo1,
-    compLineaTrabajo2,
-    recursosMetodologia,
-    recursosFormatos,
-    recursosDiagnosticos,
-    recursosHerramientas,
-    recursosMaterial,
-
+  compTitulo,
+  compImgPpal,
+  compDefinicion,
+  compVideo,
+  compDescripcion,
+  compImg1,
+  compImg2,
+  compImg3,
+  compObjetivo1,
+  compObjetivo2,
+  compObjetivo3,
+  compLineaTrabajo1,
+  compLineaTrabajo2,
+  recursosMetodologia,
+  recursosFormatos,
+  recursosDiagnosticos,
+  recursosHerramientas,
+  recursosMaterial,
 }) => {
   return (
     <>
       <div className="containerIcon">
-        <img
-          alt="Sample"
-          src={compImgPpal}
-        />
+        <img alt="Sample" src={compImgPpal} />
       </div>
 
       <div className="containerSeeComponent">
         <h1>{compTitulo}</h1>
         <h3>Definición</h3>
-        <p>
-          {compDefinicion}
-        </p>
+        <p>{compDefinicion}</p>
 
         <iframe
-        className="video"          
+          className="video"
           src={compVideo}
           title="YouTube video player"
           frameborder="0"
@@ -57,65 +50,61 @@ const ViewComponent = ({
         />
 
         <h3>Descripción del componente</h3>
-        <p>
-          {compDescripcion}
-        </p>
+        <p>{compDescripcion}</p>
 
         <div className="containerImages">
-          <img
-            width="100%"
-            alt="Sample"
-            src={compImg1}
-          />
-          <img
-            width="100%"
-            alt="Sample"
-            src={compImg2}
-          />
-          <img
-            width="100%"
-            alt="Sample"
-            src={compImg3}
-          />
+          <img width="100%" alt="Sample" src={compImg1} />
+          <img width="100%" alt="Sample" src={compImg2} />
+          <img width="100%" alt="Sample" src={compImg3} />
         </div>
 
-        <div className="containerObjetives">
+        <h3>Objetivos</h3>
+        <div className="containerObjetives">          
           <div className="objetive">
-            <h5>{compObjetivo1}</h5>
+            <p>{compObjetivo1}</p>
           </div>
 
           <div className="objetive">
-            <h5>{compObjetivo2}</h5>
+            <p>{compObjetivo2}</p>
           </div>
 
           <div className="objetive">
-            <h5>{compObjetivo3}</h5>
+            <p>{compObjetivo3}</p>
           </div>
         </div>
 
-        <div className="containerLinesWork">
+        <h3>Líneas de trabajo </h3>
+        <div className="containerLinesWork">          
           <div className="lineWork">
             <h3>{compLineaTrabajo1}</h3>
           </div>
           <div className="lineWork">
-            <h3>{compLineaTrabajo2}</h3>            
+            <h3>{compLineaTrabajo2}</h3>
           </div>
         </div>
 
         <h3>Recursos</h3>
 
         <div className="containerResources">
-          <img alt="Sample" src={Metodologia} />
-          <img alt="Sample" src={Formato} />
-          <img alt="Sample" src={Diagnostico} />
-          <img alt="Sample" src={Herramientas} />
-          <img alt="Sample" src={Material} />
+          <a target="_blank" href={recursosMetodologia}>
+            <img alt="Sample" src={Metodologia} />
+          </a>
+          <a target="_blank" href={recursosFormatos}>
+            <img alt="Sample" src={Formato} />
+          </a>
+          <a target="_blank" href={recursosDiagnosticos}>
+            <img alt="Sample" src={Diagnostico} />
+          </a>
+          <a target="_blank" href={recursosHerramientas}>
+            <img alt="Sample" src={Herramientas} />
+          </a>
+          <a target="_blank" href={recursosMaterial}>
+            <img alt="Sample" src={Material} />
+          </a>
         </div>
-        <Link to={'/home'}>
-        <button>
-          Volver
-        </button>
-        </Link>
+        {/* <Link to={"/home"}>
+          <button>Volver</button>
+        </Link> */}
       </div>
     </>
   );

90bf80900b7c237c94ae61f2b51db4a3a724a75a
Nisanech <57728976+Nisanech@users.noreply.github.com>
2023-02-16 21:37:49 -0500

    Se realiza hook personalizado para redireccionar al usuario en caso de la que sesión expire

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-import ListCourses from '../../components/ListCourses/ListCourses'
+// Componentes
+import ListCourses from "../../components/ListCourses/ListCourses";
+import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
 
 const Home = () => {
+  //* Hook personalizado para redireccionar el usuario si la sesión expira
+  useRedirectLoggedOutUser("/");
   return (
-    <>      
-      <ListCourses/>
+    <>
+      <ListCourses />
     </>
   );
 };

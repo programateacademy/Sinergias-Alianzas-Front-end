4da82c7205b7c2bc309ab2ba4a88bc3bb930a762
Jeferson Camero <jeferson.camero@cun.edu.co>
2023-03-21 17:55:44 -0500

    Space

--- src/components/seeForo/ViewForo.jsx
+++ src/components/seeForo/ViewForo.jsx
@@ -5,7 +5,6 @@ import { useState } from "react";
 import { Button, Modal, Pagination } from "react-bootstrap";
 import { motion } from "framer-motion"; //Animation library
 
-
 import searchButton from "../../components/ListCourses/Assets/searchButton.png";
 import forumimg from "../../components/ListCourses/Assets/forum.png";
 import filterimg from "../../components/ListCourses/Assets/filter.png";
@@ -61,6 +60,7 @@ const ViewComponent = ({
 
             <input type="text" placeholder="Buscar" />
           </motion.button>
+
           <Link className="link" to={"/foroa"}>
             <motion.button
               className="box1"
@@ -78,6 +78,7 @@ const ViewComponent = ({
               />
             </motion.button>
           </Link>
+
           <motion.button
             className="box1"
             whileHover={{ scale: 1.2 }}
@@ -94,6 +95,7 @@ const ViewComponent = ({
               disabled={true}
             />
           </motion.button>
+
           <motion.button
             className="box1"
             whileHover={{ scale: 1.2 }}
@@ -104,13 +106,18 @@ const ViewComponent = ({
           >
             <img src={addimg} alt="" />
 
-            <input type="text" style={{ cursor: "pointer" }} placeholder="Preguntar" disabled={true}/>
+            <input
+              type="text"
+              style={{ cursor: "pointer" }}
+              placeholder="Preguntar"
+              disabled={true}
+            />
           </motion.button>
         </div>
 
         <div className="container-fluid container_info">
           <Row>
-            <Col className="col-lg-3 col-12" style={{ marginBottom:"10px" }}>
+            <Col className="col-lg-3 col-12" style={{ marginBottom: "10px" }}>
               <Container
                 className="border-foro filter"
                 style={{ border: `2px solid ${compColor}` }}
@@ -178,58 +185,6 @@ const ViewComponent = ({
                       <img src={Coment} alt="Comentar" />
                       <p className="text_option">Respuestas</p>
                     </motion.button>
-                    <div className="model_box">
-                      <Modal
-                        show={show}
-                        onHide={handleClose}
-                        backdrop="static"
-                        keyboard={false}
-                      >
-                        <Modal.Header closeButton>
-                          <Modal.Title>Agregar Pregunta</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                          <form>
-                            <div className="form-group mt-3">
-                              <input
-                                type="text"
-                                className="form-control"
-                                id="exampleInputEmail1"
-                                aria-describedby="emailHelp"
-                                placeholder="Autor"
-                                required
-                              />
-                            </div>
-                            <br />
-                            <div className="form-group">
-                              <input
-                                type="text"
-                                className="form-control"
-                                aria-describedby="emailHelp"
-                                placeholder="Pregunta aqui"
-                                required
-                              />
-                            </div>
-                            <button
-                              type="submit"
-                              className="btn btn-success mt-4"
-                              style={{
-                                background: `${compColor}`,
-                                border: `2px solid ${compColor}`,
-                              }}
-                            >
-                              Modificar
-                            </button>
-                          </form>
-                        </Modal.Body>
-
-                        <Modal.Footer>
-                          <Button variant="secondary" onClick={handleClose}>
-                            Cerrar
-                          </Button>
-                        </Modal.Footer>
-                      </Modal>
-                    </div>
                   </Col>
                   <Col md={3} xs={12}>
                     <motion.button

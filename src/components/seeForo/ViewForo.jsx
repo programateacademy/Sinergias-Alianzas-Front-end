4571073283397ad49976df7c670fbd15be78740d
Jeferson Camero <jeferson.camero@cun.edu.co>
2023-03-21 10:43:57 -0500

    Add ButtonsForum component

--- src/components/seeForo/ViewForo.jsx
+++ src/components/seeForo/ViewForo.jsx
@@ -12,6 +12,7 @@ import Edit from "./assets/edit.png";
 import Trash from "./assets/trash.png";
 import { Pagination } from 'react-bootstrap';
 import CircularOption from "../CirculaOption/CircularOption";
+import ButtonsForum from "./ButtonsForum";
 
 const ViewComponent = ({
   compTitulo,
@@ -24,9 +25,6 @@ const ViewComponent = ({
   reportNumber,
   report,
 }) => {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   return (
     <>
       <div className="containerIcon">
@@ -122,104 +120,7 @@ const ViewComponent = ({
                 <Row>
                   <Col md={12} className="d-none d-md-block">
                     <Row className="d-flex align-items-center">
-                      <Col md={3}>
-                        <motion.button
-                          className="button_option"
-                          whileHover={{ scale: 1.04 }}
-                          onHoverStart={(e) => {}}
-                          onHoverEnd={(e) => {}}
-                        >
-                          <img src={Coment} alt="Respuestas" />
-                          <p className="text_option">Respuestas</p>
-                        </motion.button>
-                      </Col>
-                      <Col md={3}>
-                        <motion.button
-                          className="button_option"
-                          whileHover={{ scale: 1.04 }}
-                          onHoverStart={(e) => {}}
-                          onHoverEnd={(e) => {}}
-                          onClick={handleShow}
-                        >
-                          <img src={Edit} alt="Editar" />
-                          <p className="text_option">Editar</p>
-                        </motion.button>
-                        <div className="model_box">
-                          <Modal
-                            show={show}
-                            onHide={handleClose}
-                            backdrop="static"
-                            keyboard={false}
-                          >
-                            <Modal.Header closeButton>
-                              <Modal.Title>Modificar Comentario</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                              <form>
-                                <div className="form-group mt-3">
-                                  <input
-                                    type="number"
-                                    className="form-control"
-                                    id="exampleInputEmail1"
-                                    aria-describedby="emailHelp"
-                                    placeholder="Autor"
-                                    required
-                                  />
-                                </div>
-                                <br />
-                                <div className="form-group">
-                                  <input
-                                    type="text"
-                                    className="form-control"
-                                    id="addmovie_name"
-                                    aria-describedby="emailHelp"
-                                    placeholder="Comentario de usuario"
-                                    required
-                                  />
-                                </div>
-                                <button
-                                  type="submit"
-                                  className="btn btn-success mt-4"
-                                  style={{
-                                    background: `${compColor}`,
-                                    border: `2px solid ${compColor}`,
-                                  }}
-                                >
-                                  Modificar
-                                </button>
-                              </form>
-                            </Modal.Body>
-
-                            <Modal.Footer>
-                              <Button variant="secondary" onClick={handleClose}>
-                                Cerrar
-                              </Button>
-                            </Modal.Footer>
-                          </Modal>
-                        </div>
-                      </Col>
-                      <Col md={3}>
-                        <motion.button
-                          className="button_option"
-                          whileHover={{ scale: 1.04 }}
-                          onHoverStart={(e) => {}}
-                          onHoverEnd={(e) => {}}
-                        >
-                          <img src={Trash} alt="Eliminar" />
-                          <p className="text_option">Eliminar</p>
-                        </motion.button>
-                      </Col>
-                      <Col md={3}>
-                        <motion.button
-                          className="button_option"
-                          whileHover={{ scale: 1.04 }}
-                          onHoverStart={(e) => {}}
-                          onHoverEnd={(e) => {}}
-                        >
-                          <img src={Eye} alt="Ocultar" />
-                          <p className="text_option">Ocultar</p>
-                        </motion.button>
-                      </Col>
+                        <ButtonsForum compColor={compColor} />
                     </Row>
                   </Col>
                   <Col md={12} className="d-md-none">
@@ -360,64 +261,10 @@ const ViewComponent = ({
                           whileHover={{ scale: 1.04 }}
                           onHoverStart={(e) => {}}
                           onHoverEnd={(e) => {}}
-                          onClick={handleShow}
                         >
                           <img src={Edit} alt="Editar" />
                           <p className="text_option">Editar</p>
                         </motion.button>
-                        <div className="model_box">
-                          <Modal
-                            show={show}
-                            onHide={handleClose}
-                            backdrop="static"
-                            keyboard={false}
-                          >
-                            <Modal.Header closeButton>
-                              <Modal.Title>Modificar Comentario</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                              <form>
-                                <div className="form-group mt-3">
-                                  <input
-                                    type="number"
-                                    className="form-control"
-                                    id="exampleInputEmail1"
-                                    aria-describedby="emailHelp"
-                                    placeholder="Autor"
-                                    required
-                                  />
-                                </div>
-                                <br />
-                                <div className="form-group">
-                                  <input
-                                    type="text"
-                                    className="form-control"
-                                    id="addmovie_name"
-                                    aria-describedby="emailHelp"
-                                    placeholder="Comentario de usuario"
-                                    required
-                                  />
-                                </div>
-                                <button
-                                  type="submit"
-                                  className="btn btn-success mt-4"
-                                  style={{
-                                    background: `${compColor}`,
-                                    border: `2px solid ${compColor}`,
-                                  }}
-                                >
-                                  Modificar
-                                </button>
-                              </form>
-                            </Modal.Body>
-
-                            <Modal.Footer>
-                              <Button variant="secondary" onClick={handleClose}>
-                                Cerrar
-                              </Button>
-                            </Modal.Footer>
-                          </Modal>
-                        </div>
                       </Col>
                       <Col md={3}>
                         <motion.button

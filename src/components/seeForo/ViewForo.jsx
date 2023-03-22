1f278ec5764f264b9c61a8eafc25d5397a38fc66
Jeferson Camero <jeferson.camero@cun.edu.co>
2023-03-22 10:57:03 -0500

    Add modal question

--- src/components/seeForo/ViewForo.jsx
+++ src/components/seeForo/ViewForo.jsx
@@ -114,6 +114,59 @@ const ViewComponent = ({
               disabled={true}
             />
           </motion.button>
+          <div className="model_box">
+            <Modal
+              show={show}
+              onHide={handleClose}
+              backdrop="static"
+              keyboard={false}
+            >
+              <Modal.Header closeButton>
+                <Modal.Title>Agregar Pregunta</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <form>
+                  <div className="form-group mt-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Autor"
+                      required
+                    />
+                  </div>
+                  <br />
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="addmovie_name"
+                      aria-describedby="emailHelp"
+                      placeholder="Pregunta Aqui..."
+                      required
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-success mt-4"
+                    style={{
+                      background: `${compColor}`,
+                      border: `2px solid ${compColor}`,
+                    }}
+                  >
+                    Preguntar
+                  </button>
+                </form>
+              </Modal.Body>
+
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Cerrar
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </div>
         </div>
 
         <div className="container-fluid container_info">
